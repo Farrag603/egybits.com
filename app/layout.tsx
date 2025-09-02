e2deab66b5911fd491b69541ea807597bdca8df0
Mohamed-Ayman-Fahmy <muhamed.ayman.fahmy@gmail.com>
2025-09-02 17:56:28 +0300

    changesss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "../styles/index.sass";
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
+// import { Inter, Poppins } from "next/font/google";
 import cn from "classnames";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "900"],
-  variable: "--font-poppins",
-});
+// const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700", "900"],
+//   variable: "--font-poppins",
+// });
 
 export const metadata: Metadata = {
   title: "EgyBits - Software House",
@@ -41,7 +41,7 @@ export default function RootLayout({
           content="EgyBits delivers custom software solutions that transform businesses. From web applications to mobile apps, we provide cutting-edge technology services."
         />
       </head>
-      <body className={cn(inter.variable, poppins.variable)}>{children}</body>
+      <body className={ }>{children}</body>
     </html>
   );
 }
